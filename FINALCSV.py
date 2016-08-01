e301f6f7cc6fd0d8409d4a299609e9d345cc7029
ziyaxu <ziyaxu07@gmail.com>
2016-08-01 16:50:09 -0400

    pushing things

--- FINALCSV.py
+++ FINALCSV.py
@@ -1,6 +1,7 @@
 #Importing libraries
 import numpy as np
 import scipy
+import csv
 from PIL import Image
 import codecs, json
 
@@ -12,45 +13,47 @@ width = 64
 height = 64
 img = img.resize((width, height), Image.ANTIALIAS)
 
+#csv file
+with open('test.csv', 'w', newline='') as fp:
+    a = csv.writer(fp, delimiter=',')
+
 #storing as colors
-#need to do this
 arr = np.array(img)
 rgb = img.load() #array to change pixel values
 for x in range(0,width):
     for y in range(0,height):
         pix = arr[x,y]
+        yvalues = []
         if (pix[0] > pix[2]) & (pix[1] > pix[2]) & (pix[2] < 50):
-            print(pix)
             rgb[x,y] = (255,255,0,255)
-            arr[x,y] = 1
+            yvalues.append(1)
             print('yellow')
         elif (pix[2] > pix[0]) & (pix[2] > pix[1]) & (pix[2] > 50):
-            print(pix)
             rgb[x,y] = (0,0,255,255)
-            arr[x,y] = 2
+            yvalues.append(2)
             print('blue')
         elif (pix[0] > pix[1]) & (pix[0] > pix[2]) & (pix[0] > 50):
-            print(pix)
             rgb[x,y] = (255,0,0,255)
-            arr[x,y] = 3
+            yvalues.append(3)
             print('red')
         elif (pix[1] > pix[0]) & (pix[1] > pix[2]) & (pix[1] > 50):
-            print(pix)
             rgb[x,y] = (0,255,0,255)
-            arr[x,y] = 4
+            yvalues.append(4)
             print('green')
         elif (pix[3] == 0):
             rgb[x,y] = (255,255,255,255)
-            arr[x,y] = 5
+            yvalues.append(5)
             #print('white')
         elif (pix[0] == pix[1]) & (pix[1] == pix[2]) & (pix[1] <= 150):
             rgb[x,y] = (0,0,0,255)
-            arr[x,y] = 6
+            yvalues.append(6)
             #print('black')
         else:
             rgb[x,y] = (255,255,255,255)
-            arr[x,y] = 7
+            yvalues.append(7)
             #print('white')
+    a.writerows(yvalues)
+    
 
 #flipping image
 for x in range(0,width):
@@ -63,20 +66,6 @@ for x in range(0,width):
 img.save('RGBResized.png')
 
 # Write the image to arrays, using numpy
-#change this
 
 # Example of finding a specific pixel and printing it
 #print(arr[0, 5])
-
-# Printing the entire array
-#for x in arr:
-#    print (x)
-
-# Write array to a file
-b = arr.tolist() # nested lists with same data, indices
-
-build = {}
-build['image'] = b
-file_path = "FINAL.json" ## your path variable
-
-json.dump(build, open(file_path, 'w'))
