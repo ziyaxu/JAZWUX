2063667bf44fb1b38ff22a7968cedc6691985fd5
Annika Urban <urbana.gos@gmail.com>
2016-08-03 19:08:20 -0400

    Added FINALRGB.py back

---
+++ FINALRGB.py
@@ -0,0 +1,83 @@
+#FINALRGB.py
+#Importing libraries
+import numpy as np
+import scipy
+from PIL import Image
+import codecs, json
+
+# Open the image
+img = Image.open('Obama2.png')
+
+#resize image
+width = 100
+height = 100
+img = img.resize((width, height), Image.ANTIALIAS)
+
+#storing as colors
+#need to do this
+arr = np.array(img)
+rgb = img.load() #array to change pixel values
+for x in range(0,width):
+    for y in range(0,height):
+        pix = arr[x,y]
+        if (pix[0] > pix[2]) & (pix[1] > pix[2]) & (pix[2] < 50) & ((pix[1] >= (pix[0] - 50)) | (pix[1] >= (50 + pix[0]))):
+           #print(pix)
+           rgb[x,y] = (255,255,0,255)
+           arr[x,y] = 1
+           #print('yellow')
+        elif (pix[2] > pix[0]) & (pix[2] > pix[1]) & (pix[2] > 50):
+           #print(pix)
+           rgb[x,y] = (0,0,255,255)
+           arr[x,y] = 2
+           #print('blue')
+        elif (pix[0] > pix[1]) & (pix[0] > pix[2]) & (pix[0] > 50):
+           #print(pix)
+           rgb[x,y] = (255,0,0,255)
+           arr[x,y] = 3
+           #print('red')
+        elif (pix[1] > pix[0]) & (pix[1] > pix[2]) & (pix[1] > 50):
+           #print(pix)
+           rgb[x,y] = (0,255,0,255)
+           arr[x,y] = 4
+           #print('green')
+        elif (pix[3] == 0):
+           rgb[x,y] = (255,255,255,255)
+           arr[x,y] = 5
+           #print('white')
+        elif (pix[0] == pix[1]) & (pix[1] == pix[2]) & (pix[1] <= 150):
+           rgb[x,y] = (0,0,0,255)
+           arr[x,y] = 6
+           #print('black')
+        else:
+           rgb[x,y] = (255,255,255,255)
+           arr[x,y] = 7
+           #print('white')
+
+#flipping image
+for x in range(0,width):
+    for y in range(0,x):
+        temp = rgb[y,x]
+        rgb[y,x] = rgb[x,y]
+        rgb[x,y] = temp
+
+#saving
+img.save('RGBResized.png')
+
+# Write the image to arrays, using numpy
+#change this
+
+# Example of finding a specific pixel and printing it
+#print(arr[0, 5])
+
+# Printing the entire array
+#for x in arr:
+#    print (x)
+
+# Write array to a file
+b = arr.tolist() # nested lists with same data, indices
+
+build = {}
+build['image'] = b
+file_path = "FINAL.json" ## your path variable
+
+json.dump(build, open(file_path, 'w'))
